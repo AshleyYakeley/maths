3579e6a98c7b38cef6891895c0d944e66a0829e3
Ashley Yakeley <ashley@semantic.org>
2014-06-05 00:47:23 -0700

    Equivalence and quotient

--- theory/Function.v
+++ theory/Function.v
@@ -38,3 +38,70 @@ split.
 trivial.
 apply nGfG.
 Save.
+
+Class Equivalence (A:Type) : Type :=
+{
+  eqv : A -> A -> Prop;
+  equiv_reflexive : forall a, eqv a a;
+  equiv_symmetric : forall a b, eqv a b -> eqv b a;
+  equiv_transitive : forall a b c, (eqv a b /\ eqv b c) -> eqv a c
+}.
+
+Definition quotient {A} (E : Equivalence A) : set (set A) :=
+  {sa : set A| some a1 : sa, (sa = {a2 : A|eqv a1 a2})}.
+
+Definition disjoint {A} (a:set A) b : Prop := intersect a b = empty.
+
+Lemma quotient_disjoint : forall A (e : Equivalence A), all p1 : quotient e, all p2 : quotient e,
+  (p1 = p2 \/ disjoint p1 p2).
+intros.
+unfold quotient in H.
+destruct H as [a11 H].
+destruct H.
+rewrite H1. clear H1 H p1.
+
+unfold quotient in H0.
+destruct H0 as [a12 H].
+destruct H.
+rewrite H0. clear H0 H p2.
+
+destruct (lem (disjoint {a2 : A | eqv a11 a2} {a2 : A | eqv a12 a2})) as [Hd|Hnd].
+right.
+apply Hd.
+left.
+apply func_prop_ext.
+intros.
+unfold disjoint in Hnd.
+unfold intersect in Hnd.
+unfold empty in Hnd.
+assume_not.
+elim Hnd. clear Hnd.
+apply func_prop_ext.
+intros.
+split.
+intros.
+destruct H0.
+cut (eqv a11 a12).
+intro.
+elim H. clear H.
+split.
+intro.
+apply (equiv_transitive a12 a11 x).
+split.
+apply equiv_symmetric.
+apply H2.
+apply H.
+intro.
+apply (equiv_transitive a11 a12 x).
+split.
+apply H2.
+apply H.
+apply (equiv_transitive a11 x0 a12).
+split.
+apply H0.
+apply equiv_symmetric.
+apply H1.
+intro.
+contradiction H0.
+Save.
+
