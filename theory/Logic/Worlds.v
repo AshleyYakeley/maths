994ec81c8272ae76f4d32c869b7054c3eb66d7b0
Ashley Yakeley <ashley@yakeley.org>
2015-11-30 21:21:30 -0800

    Consistent Worlds

--- theory/Logic/Worlds.v
+++ theory/Logic/Worlds.v
@@ -26,3 +26,29 @@ intros.
 apply (H j).
 apply (H0 j).
 Defined.
+
+Require Import Ashley.Proposition.
+Require Import Ashley.Logic.Bool.
+
+Lemma bool_Worlds_Robust: ~ modal_collapse (bool -> Prop).
+unfold modal_collapse.
+unfold necessarily.
+unfold Worlds_Modal.
+unfold implies.
+unfold indexed_Implication.
+unfold implies.
+unfold Prop_Implication.
+unfold judge.
+unfold indexed_Judge.
+unfold judge.
+unfold Prop_Judge.
+
+apply exists_not.
+exists (fun (x:bool) => if x then True else False).
+apply exists_not.
+exists (true:bool).
+simpl.
+intro.
+firstorder.
+destruct (H false).
+Save.
